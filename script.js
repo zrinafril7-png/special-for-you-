efe31079203b6b1ed07c1be376065fd50becd2be
zrinafril7-png <zrinafril7@gmail.com>
2026-06-28 11:48:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const startBtn = document.getElementById("startBtn");
-
-startBtn.addEventListener("click", function () {
-    alert("Selamat datang 🌸\nPerjalanan kecil ini dimulai dari sini ❤️");
-});
+function mulai() {
+    alert("Selamat datang ❤️");
+}
